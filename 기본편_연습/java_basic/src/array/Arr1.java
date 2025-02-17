a0075312957935c8a04bdfc71f218b0fb4ec0006
Dseok <ehskstkrjsa@naver.com>
2025-02-17 23:29:16 +0900

    Java Basic

---
+++ 기본편_연습/java_basic/src/array/Arr1.java
@@ -0,0 +1,10 @@
+package array;
+
+public class Arr1 {
+    public static void main(String[] args) {
+        int [] scores = {90,80,70,60,50};
+        for (int i = 0; i < scores.length; i++){
+            System.out.println("Score" + (i+1) + ":" + scores[i]);
+        }
+    }
+}
